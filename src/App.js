97246fcce7858ce894d392b0e1fa7bd2de8bc32b
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2022-12-23 18:49:46 -0500

    added homepage functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React,{useState} from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
+import Homepage from './components/Pages/Homepage/Homepage';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Banner />
+      <Homepage />
       <Footer />
     </div>
   );
