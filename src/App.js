da8a239de2938d5d50487adb9a090223366f1215
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2022-12-23 19:07:14 -0500

    removed documents

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React,{useState} from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Banner from './components/Banner/Banner';
 import Homepage from './components/Pages/Homepage/Homepage';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
   return (
     <div>
       <Header />
-      <Banner />
       <Homepage />
       <Footer />
     </div>
