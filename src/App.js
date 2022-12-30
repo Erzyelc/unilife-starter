1d477462b9d7d5b8edc6ab5516824b9adf3233f0
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2022-12-30 16:30:39 -0500

    added banner section and form input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Homepage from './components/Pages/Homepage/Homepage';
+import Banner from './components/Banner/Banner';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Banner />
       <Homepage />
       <Footer />
     </div>
