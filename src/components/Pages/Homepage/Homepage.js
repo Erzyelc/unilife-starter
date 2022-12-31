b48b9d7b6410fda21c934278169a442a4b7aa856
Ryan Carpenter <ryancarpenter@mac-mini.myfiosgateway.com>
2022-12-30 19:10:25 -0500

    started api calls and homepage

--- src/components/Pages/Homepage/Homepage.js
+++ src/components/Pages/Homepage/Homepage.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Homepage.css'
+import axios from 'axios';
 
 function Homepage() {
+
+    React.useEffect(
+      ()=> {
+        axios.get(` https://unilife-server.herokuapp.com/cities?limit=9`)
+        .then(res => {
+          console.log(res.data.response)
+        })
+        .catch(err => console.log(err))
+      }
+    )
+
+
   return (
-    <div className="homepage-container">Homepage</div>
+    <div className="homepage-container">
+      <h2 className="accomodations-header">Student accomodations in our top cities</h2>
+    </div>
   )
 }
 
